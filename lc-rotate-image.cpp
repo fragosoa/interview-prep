5667ff9190e4b14ae3f5b2a864a124ebbfe9e9ef
Adolfo <fragosoadolfo1@gmail.com>
2025-08-23 14:50:30 -0600

    added problem

---
+++ lc-rotate-image.cpp
@@ -0,0 +1,19 @@
+void rotate(vector<vector<int>>& matrix) {
+        int left = 0;
+        int right = matrix.size()-1;
+
+        while(left < right){
+            for(int i=0;i<(right-left);i++){
+                int top = left;
+                int bottom = right;
+
+                int topleft = matrix[top][left+i];
+                matrix[top][left+i] = matrix[bottom-i][left];
+                matrix[bottom-i][left] = matrix[bottom][right-i];
+                matrix[bottom][right-i] = matrix[top+i][right];
+                matrix[top+i][right] = topleft;
+            }
+            right--;
+            left++;
+        }
+    }
